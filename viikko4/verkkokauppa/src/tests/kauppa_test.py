29d0a004ee457c928e72a7b48f94b3e25cf1d82d
Aaron <119842456+kahkaar@users.noreply.github.com>
2025-11-23 21:11:09 +0200

    Complete 4.4.

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -196,3 +196,29 @@ class TestKauppa(unittest.TestCase):
         self.pankki_mock.tilisiirto.assert_called_with(
             "Matti", 3, "67890", ANY, 5
         )
+
+    def test_poista_korista(self):
+        self.viitegeneraattori_mock.uusi.return_value = 42
+
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+
+        self.varasto_mock.saldo.side_effect = varasto_saldo
+        self.varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        kauppa = Kauppa(self.varasto_mock, self.pankki_mock,
+                        self.viitegeneraattori_mock)
+
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.poista_korista(1)
+        kauppa.tilimaksu("Pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with(
+            "Pekka", 42, "12345", ANY, 0
+        )
